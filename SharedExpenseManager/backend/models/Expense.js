0251962ee219ea0821dcde27d72173c910c9e846
Rifat121 <jahedulalamrifat@gmail.com>
2025-04-26 00:57:23 +0600

    Update expense model to include messid

--- SharedExpenseManager/backend/models/Expense.js
+++ SharedExpenseManager/backend/models/Expense.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 
 const ExpenseSchema = new mongoose.Schema(
   {
-    amount: {type: Number, required: true},
-    category: {type: String, required: true},
-    payer: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-    date: {type: Date, default: Date.now},
-    approved: {type: Boolean, default: false},
+    amount: { type: Number, required: true },
+    category: { type: String, required: true },
+    payer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    messId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Mess",
+      required: true,
+    },
+    date: { type: Date, default: Date.now },
+    approved: { type: Boolean, default: false },
   },
-  {timestamps: true},
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('Expense', ExpenseSchema);
